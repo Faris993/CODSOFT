e8ff10659611625f1243be31305468c3ba333421
Faris Pepic <120046218+Faris993@users.noreply.github.com>
2024-03-21 22:33:30 +0100

    Add files via upload

---
+++ Digital Clock/script.js
@@ -0,0 +1,21 @@
+function clock(){
+let monthNames = ["January","February","March","April","May","June",
+"July","August","September","October","October","November","December"];
+let dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let today = new Date();
+
+document.getElementById('Date').innerHTML = (dayNames[today.getDay()] + " " + 
+today.getDate() + " " + monthNames[today.getMonth()] + ' ' + today.getFullYear());
+
+let h = today.getHours();
+let m = today.getMinutes();
+let s = today.getSeconds();
+let day = h<11 ? 'AM' : 'PM';
+
+h = h<10 ? '0'+h : h;
+s = s<10? '0'+s: s;
+
+document.getElementById('hours').innerHTML = h;
+document.getElementById('min').innerHTML = m;
+document.getElementById('sec').innerHTML = s;
+}let inter = setInterval(clock,400);
